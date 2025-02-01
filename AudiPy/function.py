6c49cab6a4255a8cc121ceeec769c90cb3ab077c
Jake Salter <146293252+JakeSalter-Coder@users.noreply.github.com>
2025-02-01 04:24:31 -0600

    Update function.py

--- AudiPy/function.py
+++ AudiPy/function.py
@@ -9,6 +9,14 @@ def normalize_sine(min, max, data):
 def normalize_inverse(min, max, data):
     return ((max-min) * data/max(data) + min)
 
+# normalize to twelve tone
+def normalize_twelve_tone(data):
+    return np.multiply(63, np.power(2,np.divide(data,12)))
+
+# normalize by scaled
+def normalize_scaled(min, max, data):
+    return np.subtract(data,np.min(data)) / np.subtract(np.max(data),np.min(data)) * ((max-min)) + min
+
 def play(length, data):
     # total time of the track
     t = length
@@ -38,9 +46,19 @@ def play(length, data):
         previous_samples += sample_size
         data = np.append(data, amplitude * np.sin(2. * np.pi * freq * cur_samples))
 
+    previous_samples = 0
+    data_interval = np.ndarray(0)
+    for freq in Freq:
+        cur_samples = ns[previous_samples:(previous_samples+sample_size)]
+        previous_samples += sample_size
+        data_interval = np.append(data_interval, amplitude * np.sin(2. * np.pi * (500) * cur_samples))
+
+    data = np.concat(data, data_interval)
+    offset = np.min(data)
+    data = data - offset
     write("example.wav", SAMPLE_RATE, data.astype(np.int16))
 
-test = np.linspace(0, 50, 50)
-test = normalize_inverse(test)
+test = np.linspace(50, 50, 1)
+test = normalize_twelve_tone(test)
 print(test)
-play(10, test)
+play(10, [350])
