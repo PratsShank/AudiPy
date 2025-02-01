e5104673bb9f3c26282f9fedfab2a96ff06b233c
PratsShank <88939922+PratsShank@users.noreply.github.com>
2025-02-01 14:29:41 -0600

    Update process_image.py

--- examples/process_image.py
+++ examples/process_image.py
@@ -1,1 +1,10 @@
+from AudiPy.Input import read_dataset_img
 
+#defines input
+image_path = "examples/Tyler_the_creator_ac.webp"
+#converts image to numarray
+img_array = read_dataset_img(image_path)
+print(img_array)
+#this line needs to process the numarray to sound
+#output_sound_path = "output.wav"
+#print(f'Processing Complete. Sound saved to{output_sound_path}.')
