ee68fad6c265b907fbf24fa388d7a991b9587376
Nick Stephens <126211943+RobertNickStephens@users.noreply.github.com>
2025-02-01 00:05:10 -0600

    Update setup.py
    changed name and url to AudiPy to match github repo

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Audipy',
+    name='AudiPy',
     version='0.1',
     description='A simple audio file reader',
     long_description='A simple audio file reader',
     author='Nick Stephens, Group 10 hackMT 2025',
     author_email='robert.nicholas.stephens@gmail.com',
-    url='https://github.com/JustinEugene-CS/AudioFile',
+    url='https://github.com/JustinEugene-CS/AudiPy',
     packages=find_packages(),
     install_requires=[
         'numpy'
