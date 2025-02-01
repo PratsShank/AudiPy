a905d7113c66d14c08495963b594dfcd197dbd31
bereketsillassie <bereketsillassie@gmail.com>
2025-02-01 13:57:54 -0600

    Update StandardScalar.py

--- AudiPy/StandardScalar.py
+++ AudiPy/StandardScalar.py
@@ -2,25 +2,24 @@ from scipy.io.wavfile import write
 import numpy as np
 
 class StandardScalar:
-    def __init__(self, minf, maxf):
-        self.minf = minf
-        self.maxf = maxf
+    def __init__(self):
+        return
 
     def __call__(self):
         return
     
     # normalize data between the range of min to max with a sine curve
-    def normalize_sine(self):
-        return ((self.maxf-self.minf) * np.abs(np.sin(self.data)) + self.minf)
+    def normalize_sine(self, data, minf, maxf):
+        return ((maxf-minf) * np.abs(np.sin(data)) + minf)
 
     # normalize data between the range of min to max with a inverse function
-    def normalize_inverse(self):
-        return ((self.maxf-self.minf) * self.data/max(self.data) + self.minf)
+    def normalize_inverse(self, data, minf, maxf):
+        return ((maxf-minf) * data/max(data) + minf)
 
     # normalize to twelve tone
     def normalize_twelve_tone(self, data):
         return np.multiply(63, np.power(2,np.divide(data,12)))
 
     # normalize by scaled
-    def normalize_scaled(self):
-        return np.subtract(self.data,np.min(self.data)) / np.subtract(np.max(self.data),np.min(self.data)) * ((self.maxf-self.minf)) + self.minf
+    def normalize_scaled(self, data, minf, maxf):
+        return np.subtract(data,np.min(data)) / np.subtract(np.max(data),np.min(data)) * ((maxf-minf)) + minf
