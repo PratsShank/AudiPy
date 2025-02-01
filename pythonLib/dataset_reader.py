926c1c499806f194e8184ac658cd3564d115f089
Justin Eugene <36538776+JustinEugene-CS@users.noreply.github.com>
2025-01-31 23:07:27 -0600

    Update dataset_reader.py

--- pythonLib/dataset_reader.py
+++ pythonLib/dataset_reader.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
 def read_dataset_pandas(filename):
-    """Reads a CSV file into a Pandas DataFrame."""
-    return pd.read_csv(filename)
+    """Reads a CSV file into a Pandas DataFrame and converts it to a NumPy array."""
+    df = pd.read_csv(filename)
+    
+    # Convert the DataFrame to a NumPy array
+    numpy_array = df.to_numpy()
+    
+    return numpy_array
